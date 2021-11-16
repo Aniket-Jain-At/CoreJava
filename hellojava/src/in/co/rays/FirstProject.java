3fe4d94c2b065549f57161b1a494ef71077b0c70
Aniket-Jain-At <jainani62610097@gamilcom>
2021-11-16 16:44:49 +0530

    first commit

---
+++ hellojava/src/in/co/rays/FirstProject.java
@@ -0,0 +1,13 @@
+package in.co.rays;
+
+public class FirstProject {
+
+	
+	public static void main ( String[] args){
+		
+		
+	System.out.println("FirstProject.main)");
+	}
+	
+	
+}
---
+++ hellojava/src/inn/pro.java
@@ -0,0 +1,5 @@
+package inn;
+
+public class pro {
+
+}
